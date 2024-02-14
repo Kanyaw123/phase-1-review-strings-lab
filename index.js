4d860b26dc9b630a25a580bae3b4b43d6cef66bb
kanyaw <kanyawyousif243@gmail.com>
2024-02-14 11:06:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code in this file!
+let currentUser = "John";
+
+let welcomeMessage = "Welcome to Flatbook, ";
+
+welcomeMessage += currentUser;
+
+welcomeMessage += "!";
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, ";
+
+excitedWelcomeMessage += currentUser.toUpperCase();
+
+excitedWelcomeMessage += "!";
+
+let shortGreeting = "Welcome, ";
+
+shortGreeting += currentUser.charAt(0);
+
+shortGreeting += "!";
